84dcd168f453a78b7a3e9176e68b5a4e2523eb15
absjuniordev <abs.junnior@hotmail.com>
2022-09-29 21:25:39 -0300

    Instalando Pacote Externo Intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -82,7 +83,7 @@ class MyHomePage extends StatelessWidget {
                                 fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d/MMM/y').format(tr.date),
                             style: const TextStyle(color: Colors.grey),
                           )
                         ],
