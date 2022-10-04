1103cd673cc0ed690445d05c4c5751c8eec8bd71
absjuniordev <abs.junnior@hotmail.com>
2022-10-04 19:43:48 -0300

    Mudanca do Teclado generico para o Numérico

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -9,6 +9,17 @@ class TransactionForm extends StatelessWidget {
   TransactionForm(this.onSubmit,
       {super.key}); //#4.2 - depois é enviado ao construtror
 
+  _submitForm() {
+    //Função para trabalhar
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,10 +30,13 @@ class TransactionForm extends StatelessWidget {
         child: Column(children: [
           TextField(
             controller: titleController,
+            onSubmitted: (_) => _submitForm(),
             decoration: const InputDecoration(labelText: 'Titulo'),
           ),
           TextField(
             controller: valueController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            onSubmitted: (_) => _submitForm(),
             decoration: const InputDecoration(labelText: 'Valor (R\$)'),
           ),
           Row(
@@ -31,11 +45,7 @@ class TransactionForm extends StatelessWidget {
             children: [
               TextButton(
                 style: TextButton.styleFrom(foregroundColor: Colors.purple),
-                onPressed: () {
-                  final title = titleController.text;
-                  final value = double.tryParse(valueController.text) ?? 0.0;
-                  onSubmit(title, value); //#1
-                },
+                onPressed: _submitForm,
                 child: const Text('Nova Transação'),
               ),
             ],
--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -26,36 +26,6 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 211.30,
       date: DateTime.now(),
     ),
-    Transaction(
-      id: 't3',
-      title: 'Conta de #02',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Conta de #03',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta de #04',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta de #05',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't7',
-      title: 'Conta de #06',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
   ];
 
   _addTransations(String title, double value) {
@@ -75,7 +45,8 @@ class _TransactionUserState extends State<TransactionUser> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        TransactionForm(_addTransations), //(Passa uma função para receber dados
+        TransactionForm(
+            _addTransations), //Recebe os dados por contrutor e insere no '_addTransations'
         TransactionList(
             _transactions), //#2 - é chamado para mostrar tudo que contem salvo em '_transactions'
         //)#3 - fica ocioso aguardando a adção
