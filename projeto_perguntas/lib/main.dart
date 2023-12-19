540c5885478d657d04d0c3bf9cd5dd42c12cf8ea
Arnaldo Junior <abs.junnior@hotmail.com>
2023-12-18 21:01:53 -0300

    Corrigindo error do inndice

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -14,11 +14,32 @@ class PerguntaApp extends StatefulWidget {
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+  var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preta', 'Vermelho', 'Branco', 'Verde']
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Pato', 'Leão', 'Ovelha']
+    },
+    {
+      'texto': 'Qual é o seu time favorito?',
+      'respostas': ['Vitoria', 'Vasco', 'Bahia', 'Palmeiras']
+    },
+  ];
+
   responder() {
-    setState(() {
-      perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
@@ -29,23 +50,10 @@ class _PerguntaAppState extends State<PerguntaApp> {
     //   };
     // }
 
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preta', 'Vermelho', 'Branco', 'Verde']
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Coelho', 'Pato', 'Leão', 'Ovelha']
-      },
-      {
-        'texto': 'Qual é o seu time favorito?',
-        'respostas': ['Vitoria', 'Vasco', 'Bahia', 'Palmeiras']
-      },
-    ];
-
     // Lista para Widgets #01 e #02
-    List respostas = perguntas[perguntaSelecionada].cast()['respostas'];
+    List respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()['respostas']
+        : [];
     //Evolução 02
     // List<Widget> widgets = respostas
     // .map((e) => Respostas(
@@ -67,49 +75,48 @@ class _PerguntaAppState extends State<PerguntaApp> {
           centerTitle: true,
           title: const Text("Perguntas"),
         ),
-        body: Column(
-          children: [
-            Questao(
-              //indice e a chave
-              text: perguntas[perguntaSelecionada]['texto'].toString(),
-            ),
-            //"..." spread
-            //Evolução 03
-            ...respostas
-                .map((e) => Respostas(
-                      text: e,
-                      onPressed: responder,
-                    ))
-                .toList(),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: [
+                  Questao(
+                    //indice e a chave
+                    text: _perguntas[_perguntaSelecionada]['texto'].toString(),
+                  ),
+                  //"..." spread
+                  //Evolução 03
+                  ...respostas
+                      .map((e) => Respostas(text: e, onPressed: responder))
+                      .toList(),
 
-            //resumo
-            //onPressed: responder() = manda um outro executar o serviço
-            //onPressed: responder = ele mesmo executa o serviço
-            // ElevatedButton(
-            //   child: const Text('Exemplo 1'),
-            //   onPressed: () {
-            //     // invocando a função
-            //     responder();
-            //   },
-            // ),
-            // ElevatedButton(
-            //   //passando a função como parametro
-            //   onPressed: perguntaRespondida(),
-            //   child: const Text('Exemplo 2'),
-            // ),
-            // ElevatedButton(
-            //   //passando uam função como parametro
-            //   onPressed: responder,
-            //   child: const Text('Exemplo 3'),
-            // ),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     responder();
-            //   },
-            //   child: const Text('Exemplo 4'),
-            // ),
-          ],
-        ),
+                  //resumo
+                  //onPressed: responder() = manda um outro executar o serviço
+                  //onPressed: responder = ele mesmo executa o serviço
+                  // ElevatedButton(
+                  //   child: const Text('Exemplo 1'),
+                  //   onPressed: () {
+                  //     // invocando a função
+                  //     responder();
+                  //   },
+                  // ),
+                  // ElevatedButton(
+                  //   //passando a função como parametro
+                  //   onPressed: perguntaRespondida(),
+                  //   child: const Text('Exemplo 2'),
+                  // ),
+                  // ElevatedButton(
+                  //   //passando uam função como parametro
+                  //   onPressed: responder,
+                  //   child: const Text('Exemplo 3'),
+                  // ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     responder();
+                  //   },
+                  //   child: const Text('Exemplo 4'),
+                  // ),
+                ],
+              )
+            : null,
       ),
     );
   }
