185465509653f69ffbc3f738eb15e7bcd4f51ae6
Arnaldo Junior <abs.junnior@hotmail.com>
2023-12-25 21:46:23 -0300

    Refatoração para uso dos botões

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:expenses/components/transaction_user.dart';
+import 'dart:math';
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:expenses/models/transaction.dart';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
 
 void main() {
   runApp(const ExpensesApp());
@@ -23,15 +28,63 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transaction = [
+    Transaction(
+      title: "Novo Tenis",
+      value: 310.76,
+      date: DateTime.now(),
+      id: 't1',
+    ),
+    Transaction(
+      title: "Conta de Energia",
+      value: 210,
+      date: DateTime.now(),
+      id: 't2',
+    ),
+    Transaction(
+      title: "Conta de Energia",
+      value: 210,
+      date: DateTime.now(),
+      id: 't3',
+    ),
+  ];
+
+  _addTransacton(String title, double value) {
+    final newTranaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTranaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: ((context) {
+        return TransactionForm((p0, p1) {});
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
         child: const Icon(Icons.add),
       ),
       appBar: AppBar(
@@ -42,7 +95,7 @@ class MyHomePage extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _openTransactionFormModal(context),
             icon: const Icon(
               Icons.add,
               color: Colors.white,
@@ -50,17 +103,17 @@ class MyHomePage extends StatelessWidget {
           )
         ],
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(
+            const SizedBox(
               child: Card(
                 elevation: 5,
                 child: Text("Grafico"),
               ),
             ),
-            TransactionUser(),
+            TransactionList(transactions: _transaction),
           ],
         ),
       ),
