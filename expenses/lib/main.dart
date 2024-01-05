8c1e15a12536cd0adb6f6858857b86d889cd5217
Arnaldo Junior <abs.junnior@hotmail.com>
2024-01-04 22:04:23 -0300

    Definição de fontes

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -5,24 +5,38 @@ import 'package:flutter/material.dart';
 import 'components/transaction_list.dart';
 
 void main() {
-  runApp(const ExpensesApp());
+  runApp(ExpensesApp());
 }
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({super.key});
-
+  ExpensesApp({super.key});
+  final ThemeData tema = ThemeData();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // primarySwatch: Colors.purple,
-        primaryColor: Colors.purple,
-        // colorScheme: ColorScheme.fromSwatch(
-        //   accentColor: Colors.black,
-        // ),
-      ),
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
@@ -91,7 +105,9 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text(
           "Despesas Pessoais",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
         actions: [
           IconButton(
